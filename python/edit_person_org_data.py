c188e371cd963613b709739ce69ccdfad6b47d90
Ruth <ruthsander@me.com>
2022-01-28 14:26:51 +0100

    add changes

--- python/edit_person_org_data.py
+++ python/edit_person_org_data.py
@@ -14,6 +14,7 @@ from csv import reader
 import time
 import numpy as np
 import sys
+import openpyxl
 
 #  declaring all namespaces present in the xml files
 ns = {'HIS': 'http://www.example.org/ns/HIS',
@@ -22,6 +23,14 @@ ns = {'HIS': 'http://www.example.org/ns/HIS',
 
 tree = etree.parse('../github/Ibsen-Networks/xml-data/Navneregister_HISe.xml')
 regest_entries = tree.xpath('//tei:div[@xml:id and @type="person" or @type="organisation"]', namespaces=ns)
+persons_regest_entries = tree.xpath('//tei:div[@xml:id and @type="person"]', namespaces=ns)
+
+# colnames = ['XML_ID','Wikidata_ID','Viaf_ID','NHRP_ID','Given_Name','Surname','Name','Year_of_Birth','Year_of_Death','Lifespan','Instance','Gender','Country_of_citizenship','Occupation','Letters_Received','Times_Mentioned','Brief_Description']
+# person_org_col = ['XML_ID','Wikidata_ID','Viaf_ID','NHRP_ID','Given_Name','Surname','Name','Year_of_Birth','Year_of_Death','Lifespan','Instance','Gender','Country_of_citizenship','Occupation','Letters_Received','Times_Mentioned','Brief_Description']
+# work_file = pd.read_csv('Person_Register_Info.csv', names=colnames, na_filter=False)
+#
+# ids_persons = work_file.XML_ID.tolist()
+# persons_names = work_file.Name.tolist()
 
 
 def count_letters_to_person(reg_entries):
@@ -51,7 +60,7 @@ def edit_person_org_csv():
                 'Brief_Description']
     prof_col = ['Norsk','English']
     work_file = pd.read_csv('final_pers_org_details.csv', names=colnames, na_filter=False)
-    prof_file = pd.read_csv('occupations_list.csv',names=prof_col, na_filter=False)
+    prof_file = pd.read_csv('occupations_list_edited.csv',names=prof_col, na_filter=False)
 
     xmlID = work_file.XML_ID.tolist()
     wikidataID = work_file.Wikidata_ID.tolist()
@@ -126,6 +135,7 @@ def edit_person_org_csv():
               'prins':'Prince',
               'dronning':'Queen',
               'keiser': 'Emperor',
+              'keiserinne': 'Empress',
               'prinsesse': 'Princess',
               'greve': 'Count',
               'sultan': 'Sultan',
@@ -157,7 +167,16 @@ def edit_person_org_csv():
         'amerikansk':'United States of America',
         'russisk':'Russia',
         'tsjekkisk':'Czechoslovakia',
-        'britisk': 'United Kingdom of Great Britain and Ireland'
+        'britisk': 'United Kingdom of Great Britain and Ireland',
+        'osmansk': 'Ottoman Empire'
+    }
+
+    female_profs = {
+        'sangerinne': 'singer',
+        'salongvertinne':'salonnière',
+        'kvinnesaksforkjemper':'suffragette',
+        'friherrinne':'Baroness',
+        'skuespillerinne':'actor'
     }
 
     dict_occup = {}
@@ -179,12 +198,10 @@ def edit_person_org_csv():
         citi_ship = citizenship[i]
         xmlid = xmlID[i]
 
-        # add missing instnaces
+        # add missing instances
         if inst == str(''):
             key_list = []
-            for key in dict_instance_label:
-                if key in desc:
-                    key_list.append(dict_instance_label[key])
+
             if 'org' in xmlid:
                 key_list.append('organization')
                 #print(dict_instance_label[key])
@@ -213,12 +230,15 @@ def edit_person_org_csv():
             if 'Princess' in inst_list:
                 inst_list.remove('Prince')
 
+            if 'Empress' in inst_list:
+                inst_list.remove('Emperor')
+
             new_inst_list.append(inst_list)
 
         # add missing occupations
         temp_occup_list = []
-        if professions != str(''):
-            temp_occup_list.append(professions)
+        # if professions != str(''):
+        #     temp_occup_list.append(professions)
 
         if 'human' not in new_inst_list[i]:
             temp_occup_list.append('organization')
@@ -236,18 +256,44 @@ def edit_person_org_csv():
                         temp_occup_list.remove(items)
                     if items == 'historian':
                         temp_occup_list.remove(items)
-
-            if 'housewife' in temp_occup_list:
-
-                for item in temp_occup_list:
-                    if item != 'housewife':
-                        temp_occup_list.remove(item)
                     else:
                         continue
-                if len(temp_occup_list) >= 1:
+
+            if 'Princess' in temp_occup_list:
+                for items in temp_occup_list:
+                    if items == 'Prince':
+                        temp_occup_list.remove(items)
+                    else:
+                        continue
+
+            if 'Empress' in temp_occup_list:
+                for items in temp_occup_list:
+                    if items == 'Emperor':
+                        temp_occup_list.remove(items)
+                    else:
+                        continue
+
+            if 'married to' in temp_occup_list:
+                if gen == 'male':
                     for item in temp_occup_list:
-                        if item != 'housewife':
+                        if item == 'married to':
                             temp_occup_list.remove(item)
+                        else:
+                            continue
+                else:
+                    for item in temp_occup_list:
+                        if item != 'married to':
+                            temp_occup_list.remove(item)
+                        else:
+                            continue
+                    if len(temp_occup_list) >= 1:
+                        for item in temp_occup_list:
+                            if item != 'married to':
+                                temp_occup_list.remove(item)
+                    for key in female_profs:
+                        if key in desc:
+                            temp_occup_list.append(female_profs[key])
+
 
         new_occup_list.append(temp_occup_list)
 
@@ -265,28 +311,41 @@ def edit_person_org_csv():
                     new_gender_list.append('male')
 
         # add missing citizenship information
-        if citi_ship == str(''):
-            nat_list = []
-            if 'hustru' in desc:
-                nat_list.append(str(''))
-            else:
-                for k in nationality:
-                    if k in desc:
-                        nat_list.append(nationality[k])
-            new_nationality_list.append(nat_list)
+        # !Overwrite the entries from Wikidata to normalise the different
+        # names for a country, e.g. Nazi Germany -> Germany
+        # if citi_ship == str(''):
+        nat_list = []
+        if 'hustru' in desc:
+            nat_list.append(str(''))
         else:
-            new_nationality_list.append(citi_ship)
-            #print(nat_list)
+            for k in nationality:
+                if k in desc:
+                    nat_list.append(nationality[k])
+        new_nationality_list.append(nat_list)
+        # else:
+        #     new_nationality_list.append(citi_ship)
+        # print(nat_list)
+
+
+
 
 
     for x in range(len(new_nationality_list)):
         new_input = re.sub('\[', '', re.sub('\]', '', re.sub('\'', '',str(new_nationality_list[x]))))
         new_nationality_list[x]=new_input
 
+    for y in range(len(new_inst_list)):
+        new_input = re.sub('\[', '', re.sub('\]', '', re.sub('\'', '',str(new_inst_list[y]))))
+        new_inst_list[y]=new_input
+
+    for z in range(len(new_occup_list)):
+        new_input = re.sub('\[', '', re.sub('\]', '', re.sub('\'', '',str(new_occup_list[z]))))
+        new_occup_list[z]=new_input
+    # pprint.pprint(new_nationality_list)
+    print(len(new_nationality_list))
     #pprint.pprint(new_nationality_list)
     #print(len(new_nationality_list))
-    #pprint.pprint(new_nationality_list)
-    #print(len(new_nationality_list))
+
 
     return new_inst_list,new_gender_list,new_nationality_list, new_occup_list
 
@@ -295,7 +354,194 @@ n_letter_received, n_mentions = count_letters_to_person(regest_entries)
 #print(n_letter_received)
 new_i_list, new_g_list,new_n_list,new_occup_list = edit_person_org_csv()
 
-def combile_to_csv(instance, gender,citizenship,occupation,n_letter_received, n_mentions):
+
+def extract_marriages(person_entries):
+    colnames = ['XML_ID', 'Wikidata_ID', 'Viaf_ID', 'NHRP_ID', 'Given_Name', 'Surname', 'Name', 'Year_of_Birth',
+                'Year_of_Death', 'Lifespan', 'Country_of_citizenship', 'Occupation', 'Instance', 'Gender',
+                'Brief_Description']
+    wf = pd.read_csv('final_pers_org_details.csv', names=colnames, na_filter=False)
+
+    ids_persons = wf.XML_ID.tolist()
+    persons_names = wf.Name.tolist()
+
+    pers_entries = 0
+    married_to_dict = {}
+    person_id = []
+    spouse_id = []
+    for person in person_entries:
+        xml_id = person.attrib['{http://www.w3.org/XML/1998/namespace}id']
+
+        pers_entries += 1
+
+        long_desc = person.xpath('./tei:p[text()[contains(.,"gift med")]]', namespaces=ns)
+        for desc in long_desc:
+            married_to_dict[xml_id] = {}
+
+            def replace_all(text, dic):
+                for i, j in dic.items():
+                    text = text.replace(i, j)
+                return text
+
+            replace_string = {
+                '\'': '',
+                r'\n': '',
+                '&#229;': 'å',
+                '&#246;': 'ö',
+                '&#248;': 'ø',
+                '&#252;': 'ü',
+                '&amp;': '&',
+                '&#197;': 'Å'}
+
+            person_text = str(etree.tostring(desc))
+            person_texts = ' '.join(person_text.split())
+            cleaned_pers_texts = replace_all(person_texts, replace_string)
+            # print(cleaned_pers_texts)
+
+            result = re.search(r'(?<=gift med )[^.]*', cleaned_pers_texts)
+
+            try:
+                ref_pers = re.search('(?<=HIS:hisRef type="person" target=")[^">]*', str(result))
+                if ref_pers != None:
+                    # print(ref_pers.group(0))
+                    married_to_dict[xml_id]['married to'] = ref_pers.group(0)
+                    spouse_id.append(ref_pers.group(0))
+                    person_id.append(xml_id)
+                else:
+                    spouse_id.append(str(''))
+                    person_id.append(xml_id)
+
+            except TypeError:
+                spouse_id.append(str(''))
+                person_id.append(xml_id)
+                continue
+
+    # rows = zip(person_id, spouse_id)
+    # with open('married_persons.csv', 'w', ) as work_csv:
+    #     wr = csv.writer(work_csv, delimiter=',')
+    #     for row in rows:
+    #         wr.writerow(row)
+
+    workbook = openpyxl.load_workbook('./married_persons.xlsm')
+    sheet = workbook.active
+    Person_id = []
+    Spouse_id = []
+    Spouse_Name = []
+
+    for rowNum in range(2, 243):
+        Person_id.append(sheet.cell(row=rowNum, column=1).value)
+        Spouse_id.append(sheet.cell(row=rowNum, column=3).value)
+        Spouse_Name.append(sheet.cell(row=rowNum, column=5).value)
+
+    # print(Person_id)
+    # print(Spouse_id)
+    # print(Spouse_Name)
+    # ids = person.attrib['{http://www.w3.org/XML/1998/namespace}id']
+
+    for x, entry in enumerate(Spouse_id):
+        if entry == None:
+            Spouse_id[x] = str('')
+
+    for x, entry in enumerate(Spouse_Name):
+        if entry == None:
+            Spouse_Name[x] = str('')
+
+    new_married_to_list = ['Spouse\'s_Name']
+    new_married_to_id_list = ['Spouse\'s_ID']
+    #print(ids_persons)
+    print(len(ids_persons))
+    for n in range(1, 1704):
+        id = ids_persons[n]
+        # for id in ids_persons:
+
+        if id in Person_id:
+            i = Person_id.index(id)
+            if Spouse_id[i] != '' and Spouse_Name[i] != '':
+                new_married_to_id_list.append(Spouse_id[i])
+                new_married_to_list.append(Spouse_Name[i].upper())
+                # print(Spouse_id[i])
+                # print(Spouse_Name[i])
+            elif Spouse_id[i] != '' and Spouse_Name[i] == '':
+                new_married_to_id_list.append(Spouse_id[i])
+                id_tokens = Spouse_id[i].split(', ')
+                # intermediate_id_list = []
+                intermediate_name_list = []
+                # print(Spouse_id[i])
+                # print(id_tokens)
+                for token in id_tokens:
+                    if token in ids_persons:
+                        n = ids_persons.index(token)
+                        intermediate_name_list.append(persons_names[n])
+                        # print(persons_names[n])
+                    else:
+                        intermediate_name_list.append(str(''))
+                new_married_to_list.append(str(intermediate_name_list)[1:-1])
+            elif Spouse_id[i] == '' and Spouse_Name[i] != '':
+                new_married_to_list.append(Spouse_Name[i].upper())
+                if Spouse_Name[i] == 'Edvard, prins av Wales':
+                    cleaned_name = re.sub(',', '', re.sub('\)', '', re.sub('\(', '', Spouse_Name[i])))
+                else:
+                    cleaned_name = re.sub('\)','',re.sub('\(','', Spouse_Name[i]))
+                capital_names = cleaned_name.upper()
+                individual_names = capital_names.split(', ')
+                #print(individual_names)
+                intermediate_id_list = []
+                # intermediate_name_list = []
+                for name in individual_names:
+                    name_tokens = name.split(' ')
+                    #print(name)
+                    matched_ids = []
+                    for value in persons_names:
+                        v = re.sub('\]','',re.sub('\[','', value))
+                        # print(v)
+                        # for token in tokenize_name:
+                        # if all token in dict_person_reg.values():
+                        # print(dict_person_reg.values())
+                        if all(x in v for x in name_tokens):
+                            # print(list(dict_person_reg.keys())[list(dict_person_reg.values()).index(value)])
+                            print(v)
+                            index_matched_name = persons_names.index(value)
+                            print(ids_persons[index_matched_name])
+                            matched_ids.append(ids_persons[index_matched_name])
+                            #intermediate_id_list.append(ids_persons[index_matched_name])
+                        else:
+                            continue
+
+                    if len(matched_ids) ==0:
+                            intermediate_id_list.append(str('noID'))
+                    else:
+                        for match in matched_ids:
+                            intermediate_id_list.append(match)
+                new_married_to_id_list.append(intermediate_id_list)
+            else:
+                new_married_to_list.append(str(''))
+                new_married_to_id_list.append(str(''))
+        else:
+            new_married_to_id_list.append(str(''))
+            new_married_to_list.append(str(''))
+
+    for x, item in enumerate(new_married_to_id_list):
+        new_item = re.sub('\'','', re.sub('\[', '', re.sub('\]', '',str(item))))
+        new_married_to_id_list[x] = new_item
+
+    for x, item in enumerate(new_married_to_list):
+        new_item = re.sub('\'', '', str(item))
+        new_married_to_list[x] = new_item
+
+    # pprint.pprint(married_to_dict)
+    # print(Spouse_id)
+    print(new_married_to_id_list)
+    print(len(new_married_to_id_list))
+    print(new_married_to_list)
+    print(len(new_married_to_list))
+    # print(len(ids_persons))
+
+    return new_married_to_list, new_married_to_id_list
+
+
+new_married_to_list, new_married_to_id_list = extract_marriages(persons_regest_entries)
+
+
+def combile_to_csv(instance, gender,citizenship,occupation,n_letter_received, n_mentions, new_married_to_list, new_married_to_id_list):
     colnames = ['XML_ID', 'Wikidata_ID', 'Viaf_ID', 'NHRP_ID', 'Given_Name', 'Surname', 'Name', 'Year_of_Birth',
                 'Year_of_Death', 'Lifespan', 'Country_of_citizenship', 'Occupation', 'Instance', 'Gender',
                 'Brief_Description']
@@ -321,7 +567,7 @@ def combile_to_csv(instance, gender,citizenship,occupation,n_letter_received, n_
     for keys in n_mentions:
         n_mention.append(n_mentions[keys])
 
-    rows = zip(xmlID,wikidataID,viafID,nhrpID,givenName,surname,name,yearBirth,yearDeath,lifespan,instance, gender,citizenship,occupation,n_l_received, n_mention,description)
+    rows = zip(xmlID,wikidataID,viafID,nhrpID,givenName,surname,name,yearBirth,yearDeath,lifespan,instance, gender,citizenship,occupation,n_l_received, n_mention,description, new_married_to_list, new_married_to_id_list)
     with open('Person_Register_Info.csv', 'w', ) as work_csv:
         wr = csv.writer(work_csv, delimiter=',')
 
@@ -329,4 +575,4 @@ def combile_to_csv(instance, gender,citizenship,occupation,n_letter_received, n_
             wr.writerow(row)
 
 
-combile_to_csv(new_i_list, new_g_list,new_n_list,new_occup_list, n_letter_received, n_mentions)
+combile_to_csv(new_i_list, new_g_list,new_n_list,new_occup_list, n_letter_received, n_mentions, new_married_to_list, new_married_to_id_list)
