0286419e29fbad80c4479b92b579aa58cec3f8b5
Ruth <ruthsander@me.com>
2022-01-29 15:33:58 +0100

    clean up code and add comments

--- python/combine-letter-info.py
+++ python/combine-letter-info.py
@@ -1,6 +1,9 @@
 import os, glob
 import pandas as pd
 
+# THIS SCRIPT MERGES THE CONTENT OF ALL FILES NAMED 'letter_information_18' INTO A NEW CSV FILE
+
+# SPECIFY THE LOCATION OF THE CSV FILES TO BE MERGED
 path = "./"
 
 all_files = sorted(glob.glob(os.path.join(path, "letter_information_18*.csv")))
@@ -8,12 +11,3 @@ df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
 df_merged = pd.concat(df_from_each_file, ignore_index=True)
 df_merged.to_csv("merged_letter_info.csv")
 
-
-
-#
-# with open('original.csv', 'r') as f1:
-#     original = f1.read()
-#
-# with open('all.csv', 'a') as f2:
-#     f2.write('\n')
-#     f2.write(original)
